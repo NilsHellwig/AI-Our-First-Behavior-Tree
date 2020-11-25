67736feb885ed4942d3a4cc84127e6d16d73018d
Nils H <44339207+NilsHellwig@users.noreply.github.com>
2020-11-25 15:53:58 +0100

    minor changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String [] args) throws FileNotFoundException {
         Rezept myR;
-        myR = new Rezept("Pesto", "data/PestoUebung.tree");
+        myR = new Rezept("Pesto", "data/development.tree");
 
         BehaviorTree<Rezept> r;
 
